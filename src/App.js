05d8671352e50985c6e0f4b528f12b2fca8669fc
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-06-29 14:00:57 +0530

    added email js in the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { Props2 } from './components/Props2';
+import { ContactUs } from './components/emialjs';
 
 function App() {
+  const [number, setnumber] = useState(15);
+  let incNum = () => {
+    setnumber(number + 1)
+  }
+  let decNum = () => {
+    setnumber(number > 0 ? number - 1 : 0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='min-h-100vh d-flex align-items-center justfy-content-center flex-column'>
+        <h1>Number {number}</h1>
+        <div className='d-flex gap-3'>  <button onClick={incNum}>Inc Num</button>
+          <button onClick={decNum}>Dec Num</button>
+        </div>
+      </div>
+
+      <Props2 mail="sandeep@email.com" />
+      <ContactUs />
+    </>
   );
 }
 
---
+++ src/components/Props2.jsx
@@ -0,0 +1,6 @@
+export function Props2(hy) {
+    return (
+        <h2> Contact here:- {hy.mail}!</h2>
+    )
+
+}
